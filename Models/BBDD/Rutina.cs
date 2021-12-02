4f06d8fe447a09a1f38173f5d98c1f6187c1c387
arielPerez447 <64883289+ArielEzequielPerez@users.noreply.github.com>
2021-12-02 10:45:32 -0300

    actualización de readme

--- Models/BBDD/Rutina.cs
+++ Models/BBDD/Rutina.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +8,17 @@ namespace defensoresTablada.Models.BBDD
 {
     public class Rutina
     {
+        public int IdUsuario { get; set; }
+
+        [Required]
+        public string NombreDeEjercicio {get; set;}
+
+        [Required]
+        public int Serie { get; set; }
+
+        [Required]
+        public int Repeticion { get; set; }
+        
+
     }
 }
--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# defensoresTablada
- Almacena rutinas de un gimnasio
+# Descripción de la aplicacion web
+	Es una app web para un club donde un profesional cargue una rutina para un cliente y el mismo pueda ver la rutina. 
+    Ambos tienen que iniciar sesion para poder poder usarla completamente 
+
+## Requisitos
+### descargar 
+* [visual studio community](https://visualstudio.microsoft.com/es/vs/community/)
+* [SQL Server Management Studio (SSMS)](https://aka.ms/ssmsfullsetup) 
